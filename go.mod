1ae57ef189adc25693d0f9eceefdc1bccab4d43f
Pablo Chacin <pablochacin@gmail.com>
2024-06-10 19:10:46 +0200

    implement catalog interface
    Signed-off-by: Pablo Chacin <pablochacin@gmail.com>

--- go.mod
+++ go.mod
@@ -2,3 +2,12 @@ module github.com/grafana/k6catalog
 
 go 1.22.2
 
+require (
+	github.com/Masterminds/semver v1.5.0
+	github.com/spf13/cobra v1.8.0
+)
+
+require (
+	github.com/inconshreveable/mousetrap v1.1.0 // indirect
+	github.com/spf13/pflag v1.0.5 // indirect
+)
---
+++ registry.go
@@ -0,0 +1,55 @@
+package k6catalog
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"os"
+)
+
+var (
+	ErrInvalidRegistry = fmt.Errorf("invalid module registry")     //nolint:revive
+	ErrEntryNotFound   = fmt.Errorf("entry not found in registry") //nolint:revive
+)
+
+// Registry defines the interface of a module registry
+type Registry interface {
+	// GetVersion returns the versions of a a module given its name
+	GetVersions(cxt context.Context, mod string) (Entry, error)
+}
+
+// Entry defines a registry entry
+type Entry struct {
+	Module   string
+	Versions []string
+}
+
+type jsonRegistry struct {
+	Entries map[string]Entry `json:"entries"`
+}
+
+// NewJSONRegistry returns a Registry from a json file
+func NewJSONRegistry(path string) (Registry, error) {
+	buff, err := os.ReadFile(path) //nolint:forbidigo,gosec
+	if err != nil {
+		return nil, fmt.Errorf("%w: %w", ErrInvalidRegistry, err)
+	}
+	registry := jsonRegistry{}
+
+	err = json.Unmarshal(buff, &registry)
+	if err != nil {
+		return nil, fmt.Errorf("%w: %w", ErrInvalidRegistry, err)
+	}
+
+	return registry, nil
+}
+
+// GetVersions returns the versions for a given module
+func (r jsonRegistry) GetVersions(_ context.Context, mod string) (Entry, error) {
+	entry, found := r.Entries[mod]
+	if !found {
+		return Entry{}, fmt.Errorf("%w : %s", ErrEntryNotFound, mod)
+	}
+
+	return entry, nil
+}
